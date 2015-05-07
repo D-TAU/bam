e4016c31a9509b6f9fa35e73eaa77c291ae83603
Viktor Kopp <vifactor@gmail.com>
2015-05-07 23:29:46 +0300

    Added simple implementation of Account class

--- src/include/Account.h
+++ src/include/Account.h
@@ -4,11 +4,15 @@
 
 class Account
 {
-    public:
-        Account();
-        virtual ~Account();
-    protected:
-    private:
+public:
+    Account(double ibal);
+    virtual ~Account();
+
+    enum OpStatus {FAILURE, SUCCESS};
+    virtual OpStatus withdraw(double amount);
+    virtual OpStatus deposit(double amount);
+protected:
+    double m_balance;
 };
 
 #endif // ACCOUNT_H
--- src/src/Account.cpp
+++ src/src/Account.cpp
@@ -1,11 +1,30 @@
 #include "Account.h"
 
-Account::Account()
+Account::Account(double ibal)
 {
-    //ctor
+    /*setup initial account balance*/
+    m_balance = ibal;
 }
 
 Account::~Account()
 {
     //dtor
+}
+
+Account::OpStatus Account::withdraw(double amount)
+{
+    /*do not withdraw more than you have*/
+    if(amount < m_balance)
+    {
+        m_balance -= amount;
+        return SUCCESS;
+    }
+    else
+        return FAILURE;
+}
+
+Account::OpStatus Account::deposit(double amount)
+{
+    m_balance += amount;
+    return SUCCESS;
 }
